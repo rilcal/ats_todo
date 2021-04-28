479ad576c4c8e9045c15243f29173172ca6be71f
Riley <rileycallaghan96@gmail.com>
2021-04-28 16:33:47 -0400

    Update App
    add InputBox component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,91 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
 function App() {
   return (
     <div className="App">
-      <ul className="backlog">
-        <h1>1,1</h1>
-        <h1>2,1</h1>
+      <ul className="backlog" id="backlog-root">
+        <h1 className="backlog">Backlog</h1>
+        <InputBox className="backlog" />
       </ul>
-      <ul className="to-do">
-        <p></p>
-        <h1>2,2</h1>
+      <ul className="to-do" id="to-do-root">
+        <h1 className="to-do">To-Do</h1>
+        <InputBox className="to-do" />
       </ul>
-      <ul className="complete">
-        <h1>1,3</h1>
-        <h1>2,3</h1>
-        <h1>3,3</h1>
+      <ul className="complete" id="complete-root">
+        <h1 className="complete">Complete</h1>
       </ul>
     </div>
   );
 }
 
+// function InputBox() {
+//   return (
+//     <p>
+//       <input></input> <br />
+//       <input></input> <br />
+//       <button onClick={storeTask}>Add</button>
+//     </p>
+//   );
+// }
 
-function inputBox(){
-  return(
+class InputBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      className: props.className,
+      title: "",
+      desc: "",
+    };
+    this.handleTitle = this.handleTitle.bind(this);
+    this.handleDesc = this.handleDesc.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleTitle(e) {
+    this.setState((state) => ({
+      title: e.target.value,
+    }));
+  }
+
+  handleDesc(e) {
+    this.setState((state) => ({
+      desc: e.target.value,
+    }));
+  }
+
+  handleClick() {
+    console.log(this.state.className + "-root");
+    document.getElementById(this.state.className + "-root").insertBefore(() => {
+      DisplayBox(this.state.title, this.state.desc);
+    }, document.getElementById());
+  }
+
+  render() {
+    return (
+      <p>
+        <input
+          onChange={(e) => {
+            this.handleTitle(e);
+          }}
+        ></input>{" "}
+        <br />
+        <input onChange={this.handleDesc}></input> <br />
+        <button onClick={this.handleClick}>Add</button>
+      </p>
+    );
+  }
+}
+
+function DisplayBox(title, desc) {
+  render(
     <p>
-      
-
+      <h3>{title}</h3>
+      <h6>{desc}</h6>
     </p>
   );
 }
+
+function storeTask(par) {}
+
 export default App;
