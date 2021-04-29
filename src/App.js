0532af27a78eb7d474349b4715475a0076fedf53
Riley <rileycallaghan96@gmail.com>
2021-04-28 22:54:26 -0400

    try my best
    this is as far as I went using multible component classes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,56 @@
-import { render } from "@testing-library/react";
 import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <ul className="backlog" id="backlog-root">
-        <h1 className="backlog">Backlog</h1>
-        <InputBox className="backlog" />
-      </ul>
-      <ul className="to-do" id="to-do-root">
-        <h1 className="to-do">To-Do</h1>
-        <InputBox className="to-do" />
-      </ul>
-      <ul className="complete" id="complete-root">
-        <h1 className="complete">Complete</h1>
-      </ul>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { toToDo: "", toComplete: "" };
+  }
+
+  update() {
+    this.setState({
+      toToDo: this.backlog,
+      toComplete: this.complete,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ul className="backlog" id="backlog-root">
+          <Column
+            ref={(component) => (this.backlog = component)}
+            columnName="Backlog"
+            className="backlog"
+            tasks={this.backlogTasks}
+          />
+          <Column
+            ref={(component) => (this.toDo = component)}
+            columnName="To-Do"
+            className="to-do"
+            tasks={this.toDoTasks}
+          />
+          <CompleteColumn
+            ref={(component) => (this.complete = component)}
+            columnName="Complete"
+            className="complete"
+            tasks={this.completeTasks}
+          />
+        </ul>
+      </div>
+    );
+  }
 }
 
-// function InputBox() {
-//   return (
-//     <p>
-//       <input></input> <br />
-//       <input></input> <br />
-//       <button onClick={storeTask}>Add</button>
-//     </p>
-//   );
-// }
-
-class InputBox extends React.Component {
+class Column extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      columnName: props.columnName,
       className: props.className,
       title: "",
       desc: "",
+      tasks: [],
+      update: false,
     };
     this.handleTitle = this.handleTitle.bind(this);
     this.handleDesc = this.handleDesc.bind(this);
@@ -54,38 +69,91 @@ class InputBox extends React.Component {
     }));
   }
 
-  handleClick() {
-    console.log(this.state.className + "-root");
-    document.getElementById(this.state.className + "-root").insertBefore(() => {
-      DisplayBox(this.state.title, this.state.desc);
-    }, document.getElementById());
+  handleClick(e) {
+    this.setState(() => ({
+      update: true,
+    }));
+    console.log(e);
+    this.state.tasks.push(
+      <DisplayBox title={this.state.title} desc={this.state.desc} />
+    );
+    this.setState(() => ({
+      update: false,
+    }));
   }
 
   render() {
     return (
-      <p>
-        <input
-          onChange={(e) => {
-            this.handleTitle(e);
-          }}
-        ></input>{" "}
-        <br />
-        <input onChange={this.handleDesc}></input> <br />
-        <button onClick={this.handleClick}>Add</button>
-      </p>
+      <ul>
+        <section>
+          <h1>{this.state.columnName}</h1>
+        </section>
+        <section>
+          {this.state.tasks.map((task) => {
+            return task;
+          })}
+        </section>
+        <section>
+          <input
+            onChange={(e) => {
+              this.handleTitle(e);
+            }}
+          ></input>
+          <br />
+          <input onChange={this.handleDesc}></input> <br />
+          <button onClick={this.handleClick}>Add</button>
+        </section>
+      </ul>
     );
   }
 }
 
-function DisplayBox(title, desc) {
-  render(
-    <p>
-      <h3>{title}</h3>
-      <h6>{desc}</h6>
-    </p>
-  );
+class CompleteColumn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      columnName: props.columnName,
+      className: props.className,
+      title: "",
+      desc: "",
+      tasks: [],
+      update: false,
+    };
+  }
+
+  render() {
+    return (
+      <ul>
+        <section>
+          <h1>{this.state.columnName}</h1>
+        </section>
+        <section>
+          {this.state.tasks.map((task) => {
+            return task;
+          })}
+        </section>
+      </ul>
+    );
+  }
 }
 
-function storeTask(par) {}
+class DisplayBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: props.title,
+      desc: props.desc,
+    };
+  }
+  render() {
+    return (
+      <ul>
+        <label>{this.state.title}</label>
+        <label>{this.state.desc}</label>
+        <button>Delete</button>
+      </ul>
+    );
+  }
+}
 
 export default App;
