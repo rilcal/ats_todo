5814537ca9f6a20cbcbd40b87de3e9025acb6acb
Riley <rileycallaghan96@gmail.com>
2021-04-29 07:16:42 -0400

    Use large App component
    ditch columns idea

--- src/App.js
+++ src/App.js
@@ -3,7 +3,77 @@ import React from "react";
 class App extends React.Component {
   constructor() {
     super();
-    this.state = { toToDo: "", toComplete: "" };
+    this.state = {
+      backlogTasks: [],
+      toDoTasks: [],
+      completeTasks: [],
+      backlogTitle: "",
+      backlogDesc: "",
+      toDoTitle: "",
+      toDoDesc: "",
+      toggle: false,
+    };
+  }
+
+  toggle() {
+    if (this.state.toggle) {
+      this.setState({
+        toggle: false,
+      });
+    } else {
+      this.setState({
+        toggle: true,
+      });
+    }
+  }
+
+  handleBacklogTitle(e) {
+    this.setState({
+      backlogTitle: e.target.value,
+    });
+  }
+
+  handleBacklogDesc(e) {
+    this.setState({
+      backlogDesc: e.target.value,
+    });
+  }
+
+  handleBacklogClick() {
+    this.state.backlogTasks.push(
+      <DisplayBox
+        title={this.state.backlogTitle}
+        desc={this.state.backlogDesc}
+        delete={this.delete.bind(this)}
+      />
+    );
+    this.toggle();
+  }
+
+  handleToDOTitle(e) {
+    this.setState({
+      toDoTitle: e.target.value,
+    });
+  }
+
+  handleToDODesc(e) {
+    this.setState({
+      toDoDesc: e.target.value,
+    });
+  }
+
+  delete(rmvcomp) {
+    let i = 0;
+    this.state.backlogTasks.forEach((comp) => {
+      console.log(comp.props.title);
+      console.log(rmvcomp.props.title);
+      console.log(comp.props.title === rmvcomp.props.title);
+      if (comp.props.title === rmvcomp.props.title) {
+        this.state.backlogTasks.splice(i, 1);
+        this.toggle();
+        return;
+      }
+    });
   }
 
   update() {
@@ -16,25 +86,66 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <ul className="backlog" id="backlog-root">
-          <Column
-            ref={(component) => (this.backlog = component)}
-            columnName="Backlog"
-            className="backlog"
-            tasks={this.backlogTasks}
-          />
-          <Column
-            ref={(component) => (this.toDo = component)}
-            columnName="To-Do"
-            className="to-do"
-            tasks={this.toDoTasks}
-          />
-          <CompleteColumn
-            ref={(component) => (this.complete = component)}
-            columnName="Complete"
-            className="complete"
-            tasks={this.completeTasks}
-          />
+        <ul>
+          <section>
+            <h1>Backlog</h1>
+          </section>
+          <section>
+            {this.state.backlogTasks.map((task) => {
+              return task;
+            })}
+          </section>
+          <section>
+            <input
+              onChange={(e) => {
+                this.handleBacklogTitle(e);
+              }}
+            ></input>
+            <br />
+            <input
+              onChange={(e) => {
+                this.handleBacklogDesc(e);
+              }}
+            ></input>
+            <br />
+            <button
+              onClick={() => {
+                this.handleBacklogClick();
+              }}
+            >
+              Add
+            </button>
+          </section>
+        </ul>
+        <ul>
+          <section>
+            <h1>To-Do</h1>
+          </section>
+          <section>
+            {this.state.toDoTasks.map((task) => {
+              return task;
+            })}
+          </section>
+          <section>
+            <input
+              onChange={(e) => {
+                this.handleTitle(e);
+              }}
+            ></input>
+            <br />
+            <input onChange={this.handleDesc}></input> <br />
+            <button onClick={this.handleClick}>Add</button>
+          </section>
+        </ul>
+        <ul>
+          <section>
+            <h1>Complete</h1>
+          </section>
+          <section>
+            {this.state.completeTasks.map((task) => {
+              return task;
+            })}
+          </section>
         </ul>
       </div>
     );
@@ -150,7 +261,13 @@ class DisplayBox extends React.Component {
       <ul>
         <label>{this.state.title}</label>
         <label>{this.state.desc}</label>
-        <button>Delete</button>
+        <button
+          onClick={() => {
+            this.props.delete(this);
+          }}
+        >
+          Delete
+        </button>
       </ul>
     );
   }
